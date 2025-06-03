4ecfa87a3bb7db50fa524f6cede779f746df672b
linyueCS-IT <6197673@johnabbottcollege.net>
2025-06-03 09:52:59 -0400

    Create new Fields and Constructor
    - remove xml related data
    - create new ew Fields and Constructor

--- Budget/Services/Categories.cs
+++ Budget/Services/Categories.cs
@@ -14,79 +14,41 @@ namespace Budget.Services
 {
     public class Categories
     {
-        private static string DefaultFileName = "budgetCategories.txt";
-        private List<Category> _Cats = new List<Category>();
-        private string _FileName;
-        private string _DirName;
 
-
-
-        #region property        
-        public string FileName { get { return _FileName; } }
-        public string DirName { get { return _DirName; } }
+        #region Private Fields
+        private DatabaseService _databaseService;
+        private readonly bool _ownsDatabase;
+        private bool _disposed = false;
         #endregion
 
-        #region contructor
-        public Categories()
-        {
-            SetCategoriesToDefaults();
-        }
+        #region Public Properties    
+        public string DatabasePath => Path.GetFullPath(_databaseService?.Connection?.DataSource ?? "");
+        public bool IsConnected => _databaseService?.Connection?.State == System.Data.ConnectionState.Open;
 
-
-        public Category GetCategoryFromId(int i)
-        {
-            Category c = _Cats.Find(x => x.Id == i);
-            if (c == null)
-            {
-                throw new Exception("Cannot find category with id " + i.ToString());
-            }
-            return c;
-        }
         #endregion
 
-
-        public void ReadFromFile(string filepath = null)
+        #region Constructors
+        public Categories(DatabaseService databaseService)
         {
-
-            // reading from file resets all the current categories,
-            _Cats.Clear();
-
-            // reset default dir/filename to null 
-            // ... filepath may not be valid, 
-            _DirName = null;
-            _FileName = null;
-
-
-            filepath = BudgetFiles.VerifyReadFromFileName(filepath, DefaultFileName);
-
-            // If file exists, read it
-            _ReadXMLFile(filepath);
-            _DirName = Path.GetDirectoryName(filepath);
-            _FileName = Path.GetFileName(filepath);
+            _databaseService = databaseService ?? throw new ArgumentNullException(nameof(databaseService));
+            _ownsDatabase = false; // External ownership
         }
-        public void SaveToFile(string filepath = null)
-        {
 
-            // if file path not specified, set to last read file
-            if (filepath == null && DirName != null && FileName != null)
+        public Categories(string databasePath, bool isNew = false)
+        {
+            if (string.IsNullOrWhiteSpace(databasePath)) throw new ArgumentNullException(nameof(databasePath));
+            if (isNew)
             {
-                filepath = DirName + "\\" + FileName;
+                _databaseService = DatabaseService.CreateNewDatabase(databasePath);
+                SetCategoriesToDefaults(); // Automatically set up default categories for new database
             }
-
-            // just in case filepath doesn't exist, reset path info
-            _DirName = null;
-            _FileName = null;
-
-            // get filepath name (throws exception if it doesn't exist)
-            filepath = BudgetFiles.VerifyWriteToFileName(filepath, DefaultFileName);
-
-            // save as XML
-            _WriteXMLFile(filepath);
-
-            // save filename info for later use
-            _DirName = Path.GetDirectoryName(filepath);
-            _FileName = Path.GetFileName(filepath);
+            else
+            {
+                _databaseService = DatabaseService.OpenExisting(databasePath);
+            }
+            _ownsDatabase = true; // This instance owns the database
         }
+        #endregion
 
         public void SetCategoriesToDefaults()
         {
@@ -148,86 +110,6 @@ namespace Budget.Services
         }
 
 
-        private void _ReadXMLFile(string filepath)
-        {
-            try
-            {
-                XmlDocument doc = new XmlDocument();
-                doc.Load(filepath);
-
-                foreach (XmlNode category in doc.DocumentElement.ChildNodes)
-                {
-                    string id = ((XmlElement)category).GetAttributeNode("ID").InnerText;
-                    string typestring = ((XmlElement)category).GetAttributeNode("type").InnerText;
-                    string desc = ((XmlElement)category).InnerText;
-
-                    Category.CategoryType type;
-                    switch (typestring.ToLower())
-                    {
-                        case "income":
-                            type = Category.CategoryType.Income;
-                            break;
-                        case "expense":
-                            type = Category.CategoryType.Expense;
-                            break;
-                        case "debt":
-                            type = CategoryType.Debt;
-                            break;
-                        case "investment":
-                            type = CategoryType.Investment;
-                            break;
-                        case "savings":
-                            type = CategoryType.Savings;
-                            break;
-                        default:
-                            type = CategoryType.Expense;
-                            break;
-                    }
-                    Add(new Category(int.Parse(id), desc, type));
-                }
-
-            }
-            catch (Exception e)
-            {
-                throw new Exception("ReadXMLFile: Reading XML " + e.Message);
-            }
-
-        }
-
-        private void _WriteXMLFile(string filepath)
-        {
-            try
-            {
-                // create top level element of categories
-                XmlDocument doc = new XmlDocument();
-                doc.LoadXml("<Categories></Categories>");
-
-                // foreach Category, create an new xml element
-                foreach (Category cat in _Cats)
-                {
-                    XmlElement ele = doc.CreateElement("Category");
-                    XmlAttribute attr = doc.CreateAttribute("ID");
-                    attr.Value = cat.Id.ToString();
-                    ele.SetAttributeNode(attr);
-                    XmlAttribute type = doc.CreateAttribute("type");
-                    type.Value = cat.Type.ToString();
-                    ele.SetAttributeNode(type);
-
-                    XmlText text = doc.CreateTextNode(cat.Name);
-                    doc.DocumentElement.AppendChild(ele);
-                    doc.DocumentElement.LastChild.AppendChild(text);
-
-                }
-
-                // write the xml to FilePath
-                doc.Save(filepath);
-
-            }
-            catch (Exception e)
-            {
-                throw new Exception("_WriteXMLFile: Reading XML " + e.Message);
-            }
-        }
+       
     }
 }
-
